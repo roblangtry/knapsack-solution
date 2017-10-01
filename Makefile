ae40ef5d532a65f3d1ae853c538fcb47c480dd53
Robert Langtry <rob.langtry2@gmail.com>
2017-10-01 18:15:15 +1100

    Added debug print commands

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+sequential_debug:
+	gcc src/sequential.c -O3 -D DEBUG -o out/sequential
+
+build_debug: sequential_debug
 
 sequential:
 	gcc src/sequential.c -O3 -o out/sequential
--- src/sequential.c
+++ src/sequential.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#if DEBUG
+#define LOG(x) fprintf(stderr, "%s", x)
+#define LOG_INT(x) fprintf(stderr, "%d", x)
+#else
+#define LOG(x)
+#define LOG_INT(x)
+#endif
+
 int
 main(int argc, char *argv[])
 {
-    printf("Hello World\n");
+    LOG("Hello World\n");
     return 0;
 }
