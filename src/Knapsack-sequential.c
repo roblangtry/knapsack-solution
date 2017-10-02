590422846426c3bc90529e977b0400a7a00203a6
ryshjx <ryshjx@users.noreply.github.com>
2017-10-02 16:50:49 +1100

    Annie's sequential code

---
+++ src/Knapsack-sequential.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// The input argument is "<number of Item> <weight of the bag> <weight of item1 (w1)> <value of item1 (v1)> <w2> <v2> ..."
+int main(int argc, char *argv[]){
+
+	const int itemNum;
+	const int maxWeight;
+
+	if((argc-1)%2!=0){
+		printf("The number of input arguments is wrong. Maybe you missed an argument for an item. Please try again.");
+		return 0;
+	}
+
+	sscanf(argv[1],"%i",&itemNum);
+	sscanf(argv[2],"%i",&maxWeight);
+
+	struct item{
+		int weight;
+		int value;
+	}items[itemNum+1]; //item1=item[1], item[0] is not used.
+
+	int m[itemNum+1][maxWeight+1];
+
+	for(int i=1;i<itemNum+1;i++){
+		sscanf(argv[i*2+1],"%i",&items[i].weight);
+		sscanf(argv[i*2+2],"%i",&items[i].value);
+	}
+
+	for(int i=0;i<itemNum+1;i++){
+		for(int j=0;j<maxWeight+1;j++){
+			if(i==0 || j==0){
+				m[i][j]=0;
+			}
+		}
+	}
+
+	for(int i=1;i<itemNum+1;i++){
+		for(int w=1;w<maxWeight+1;w++){
+			if(items[i].weight<=k && (m[i-1][k-items[i].weight]+items[i].value > m[i-1][k])){
+				m[i][k]=m[i-1][k-items[i].weight]+items[i].value;				
+			}else{
+				m[i][k]=m[i-1][k];
+			}
+		}
+	}
+
+	printf("The max value is %d \n", m[itemNum][maxWeight]);
+	return 0;
+}
