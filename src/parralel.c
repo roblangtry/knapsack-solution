5c7e37bd1d0dc74e6097e36e6b72ee6655e9437e
roblangtry <rob.langtry2@gmail.com>
2017-10-17 14:33:41 +1100

    Returning to the drawing board as i ahve hit a wall

--- src/parralel.c
+++ src/parralel.c
@@ -25,6 +25,11 @@
 #define LOG_INT(x)
 #endif
 
+/* from https://stackoverflow.com/questions/3437404/min-and-max-in-c */
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+
 typedef struct {
     int value;
     int weight;
@@ -36,7 +41,7 @@ void sequential_process_objects(int map_size, int * map, FILE * fp);
 int read_batch(object_t * batch, FILE * fp);
 void map_batch(object_t * batch, int batch_size, int ** pmaps, int * map, int map_size);
 void consolidate_batch(int ** pmaps, int * map, int map_size);
-void parrallel_process_objects(int map_size, int * map, FILE * fp);
+int parrallel_process_objects(int map_size, FILE * fp);
 
 
 int
@@ -49,9 +54,7 @@ main(int argc, char *argv[])
     LOG("INFO: Knapsack size - ");
     LOG_INT(knapsack_size);
     LOG("\n");
-    map = calloc(knapsack_size + 1, sizeof(int));
-    process_objects(knapsack_size, map, fp);
-    free(map);
+    printf("%d\n", parrallel_process_objects(knapsack_size, fp));
     return 0;
 }
 
@@ -86,24 +89,24 @@ initialise(int argc, char *argv[], int *size, FILE ** fp)
     }
 }
 
-void process_objects(int map_size, int * map, FILE * fp){
-    if(THREADS > 1)
-        parrallel_process_objects(map_size, map, fp);
-    else
-        sequential_process_objects(map_size, map, fp);
-}
 
-void sequential_process_objects(int map_size, int * map, FILE * fp)
+int parrallel_process_objects(int map_size, FILE * fp)
 {
     LOG("INFO: ");
     LOG_INT(THREADS);
-    LOG(" threads requested using sequential solution\n");
+    LOG(" threads requested using parrallel solution\n");
     /* Knapsack solution from https://en.wikipedia.org/wiki/Knapsack_problem#0.2F1_knapsack_problem */
-    int value, weight, pval = 10,i;
-    while(fscanf(fp, "%d %d", &value, &weight)>0)
+    int value, weight, pval = 10,i,o;
+    object_t * objects = malloc(sizeof(object_t)*BATCH_SIZE);
+    object_t object;
+    int list_size = BATCH_SIZE;
+    int occupancy = 0;
+    int ** table;
+    int row,col;
+    while(fscanf(fp, "%d %d", &object.value, &object.weight)>0)
     {
         /* Check the value is valid */
-        if(weight <= 0 || weight > map_size)
+        if(object.weight <= 0 || object.weight > map_size)
         {
             /*
             The value isn't valid either because:
@@ -111,9 +114,9 @@ void sequential_process_objects(int map_size, int * map, FILE * fp)
                 * it is larger than the knapsack size and therefore won't fit
              */
             LOG("WARNING: Invalid object with value \'");
-            LOG_INT(value);
+            LOG_INT(object.value);
             LOG("\' of weight \'");
-            LOG_INT(weight);
+            LOG_INT(object.weight);
             LOG("\' read from input.\n");
         }
         else
@@ -123,134 +126,48 @@ void sequential_process_objects(int map_size, int * map, FILE * fp)
             {
                 /* Some logging to allow debugging */
                 LOG("INFO: Object with value \'");
-                LOG_INT(value);
+                LOG_INT(object.value);
                 LOG("\' of weight \'");
-                LOG_INT(weight);
+                LOG_INT(object.weight);
                 LOG("\' read from input.\n");
                 pval--;
                 if(!pval)
                     LOG("INFO: Further input will be supressed...\n");
 
             }
-            /* Do processing */
-            for(i = map_size; i>=weight;i--)
+            objects[occupancy++] = object;
+            if(occupancy == list_size)
             {
-                if(weight <= i)
-                {
-                    if(map[i] < map[i-weight]+value)
-                        map[i] = map[i-weight]+value;
-                }
+                list_size = list_size << 1;
+                objects = realloc(objects, list_size*sizeof(object_t));
             }
         }
     }
-    printf("%d\n", map[map_size]);
-}
-
-int read_batch(object_t * batch, FILE * fp)
-{
-    int size = 0;
-    // /printf("READ\n");
-    while(size < BATCH_SIZE){
-        if(fscanf(fp, "%d %d", &batch[size].value, &batch[size].weight) != 2){
-            // /printf("INC\n");
-            return size;
-        }
-        else{
-            // /printf("FUL\n");
-            size++;
-        }
+    table = malloc(occupancy*sizeof(int *));
+    for(i=0;i<occupancy;i++){
+        table[i]=malloc((1+map_size)*sizeof(int));
     }
-    return size;
-}
-
-void map_object(int weight, int value, int * map, int map_size)
-{
-    int i;
-    for(i = map_size; i >= weight; i--)
-    {
-        if(weight <= i)
+    for(row=0;row<occupancy;row++){
+        weight = objects[row].weight;
+        value = objects[row].value;
+        if(!row)
         {
-            if(map[i] < map[i-weight]+value)
-                map[i] = map[i-weight]+value;
+            for(col=0;col<weight;col++){
+                table[row][col]=0;
+            }
+            for(col=weight;col<map_size;col++){
+                table[row][col]=value;
+            }
         }
-    }
-}
-
-void map_batch(object_t * batch, int batch_size, int ** batch_maps, int * map, int map_size)
-{
-    int t_no,i;
-    #pragma omp parallel num_threads(THREADS) private(t_no,i)
-    {
-        t_no = omp_get_thread_num();
-        for(i=t_no; i < batch_size ; i = i + THREADS)
+        else
         {
-            map_object(batch[i].weight, batch[i].value, batch_maps[t_no], map_size);
+            for(col=0;col<weight;col++){
+                table[row][col]=table[row-1][col];
+            }
+            for(col=weight;col<map_size;col++){
+                table[row][col]=MAX(table[row-1][col],value+table[row-1][col-weight]);
+            }
         }
     }
-}
-
-void reconcile_map(int weight, int value, int * map, int map_size, int * origin)
-{
-    int i;
-    for(i = map_size; i >= weight; i--)
-    {
-        if(weight <= i)
-        {
-            if(map[i] < origin[i-weight]+value)
-                map[i] = origin[i-weight]+value;
-        }
-    }
-}
-
-void reconcile(int * thread_map, int * map, int map_size)
-{
-    int j;
-    int * shadow = malloc((map_size + 1) * sizeof(int));
-    memcpy(shadow, map, (map_size + 1) * sizeof(int));
-    for(j=1; j<map_size; j++)
-    {
-        if(thread_map[j])
-            reconcile_map(j, thread_map[j], map, map_size, shadow);
-    }
-    free(shadow); 
-}
-
-void consolidate_batch(int ** batch_maps, int * map, int map_size)
-{
-    int i;
-    for(i=0;i<THREADS;i++)
-    {
-        reconcile(batch_maps[i], map, map_size);
-    }
-}
-
-void parrallel_process_objects(int map_size, int * map, FILE * fp)
-{
-    LOG("INFO: ");
-    LOG_INT(THREADS);
-    LOG(" threads requested using parrallel solution\n");
-    int size,i;
-    // /printf("BATCHMAPS\n");
-    int ** batch_maps = malloc(sizeof(int *) * THREADS);
-    for(i=0;i<THREADS;i++)
-    {
-        batch_maps[i] = calloc(map_size, sizeof(int));
-    }
-    // /printf("PREP\n");
-    object_t batch[BATCH_SIZE];
-    size = read_batch(batch, fp);
-    while(size == BATCH_SIZE)
-    {
-        // /printf("MAP\n");
-        map_batch(batch, size, batch_maps, map, map_size);
-        size = read_batch(batch, fp);
-    }
-    if(size)
-    {
-        // /printf("MAP\n");
-        map_batch(batch, size, batch_maps, map, map_size);
-    }
-    // /printf("CON\n");
-    consolidate_batch(batch_maps, map, map_size);
-    printf("%d\n", map[map_size]);
+    return table[occupancy-1][map_size-1];
 }
